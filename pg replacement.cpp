4c2bbcca84a209c6ca7bbb500436b8c2a23350de
Hannah Speer <32077149+avizenith@users.noreply.github.com>
2023-12-01 15:15:52 -0600

    file read

--- pg replacement.cpp
+++ pg replacement.cpp
@@ -4,6 +4,10 @@
 
 using namespace std; 
 
+//funct identifiers
+void read(string fn);
+
+vector<int> inp;
 
 //OPEN/READ FILES
 //READ FIRST LETTER
@@ -11,14 +15,36 @@ using namespace std;
 //READ STRING
 
 int main() {
-    vector<int> inp;
-    string filename = "";
+    
+    string filename = "FIFO.txt";
+    read(filename);
 }
 
+//read file
 void read(string fn){
     fstream fin(fn);
     char i;
+    char algo;
+    //read letter designating mode
+    algo = fin.get();
+    fin.ignore();
+
+    //cycle thru file and assign numbers to vector  
     while(fin >> i){
-        cout << i; 
+        //add i to array
+        //convert char to int
+        inp.push_back(i - 48);
+        //skip delimiter
+        fin.ignore();
+
     }
+    cout << "\n\n";
+    for (const int& i : inp) {
+        cout << i << "  ";
+    }
+
+    //TEMP JUST TO SEE BEFORE IT CLOSES
+    int x;
+    cout << "Type a number: "; // Type a number and press enter
+    cin >> x; // Get user input from the keyboard
 }
